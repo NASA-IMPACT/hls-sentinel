e82677961e7bc4784bfdc28f4e6c726644728221
sharkinsspatial <sharkinsgis@gmail.com>
2022-09-07 13:10:53 -0500

    fix: Make all objects public when copying to debug bucket.

--- scripts/sentinel.sh
+++ scripts/sentinel.sh
@@ -209,7 +209,7 @@ else
   # Copy all intermediate files to debug bucket.
   echo "Copy files to debug bucket"
   debug_bucket_key=s3://${debug_bucket}/${outputname}
-  aws s3 cp "$workingdir" "$debug_bucket_key" --recursive
+  aws s3 cp "$workingdir" "$debug_bucket_key" --recursive --acl public-read
 fi
 
 # Generate GIBS browse subtiles
@@ -246,7 +246,7 @@ for gibs_id_dir in "$gibs_dir"/* ; do
         # Copy all intermediate files to debug bucket.
         echo "Copy files to debug bucket"
         debug_bucket_key=s3://${debug_bucket}/${outputname}
-        aws s3 cp "$gibs_id_dir" "$debug_bucket_key" --recursive --quiet
+        aws s3 cp "$gibs_id_dir" "$debug_bucket_key" --recursive --quiet --acl public-read
       fi
     fi
 done
